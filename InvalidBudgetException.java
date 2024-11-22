948fa1cf93cb528382a9af13801bfa60a93ac8f9
Darshan-T-P <darshantp13@gmail.com>
2024-11-22 19:44:12 +0530

    Files added

--- InvalidBudgetException.java
+++
@@ -1,9 +0,0 @@
-package expensetracker;
-
-
-// Custom exception for invalid budget
-public class InvalidBudgetException extends Exception {
-    public InvalidBudgetException(String message) {
-        super(message);  // Pass the error message to the Exception class
-    }
-}
--- InvalidIndexException.java
+++
@@ -1,8 +0,0 @@
-package expensetracker;
-
-// Custom exception for invalid index
-public class InvalidIndexException extends Exception {
-    public InvalidIndexException(String message) {
-        super(message);  // Pass the error message to the Exception class
-    }
-}
